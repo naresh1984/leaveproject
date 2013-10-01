949175ffebea7fa44ac41af03075bdb6512781af
naresh1984 <naresh.nookal@gmail.com>
2013-10-01 11:56:33 +0530

    m

--- db/migrate/20131001061928_leave_id_to_leave_requests.rb
+++ db/migrate/20131001061928_leave_id_to_leave_requests.rb
@@ -1,8 +1,14 @@
 class LeaveIdToLeaveRequests < ActiveRecord::Migration
-  def up
- change_column :leave_requests, :employee_id, :integer
+
+ def change
+  
+    change_column :leave_requests, :employee_id, :integer
+   
+
+
+  
   end
 
-  def down
-  end
+
+
 end
