e55c48842ede6d4ba00e436b8b9d7ea681d1d53d
naresh1984 <naresh.nookal@gmail.com>
2013-10-03 17:28:45 +0530

    m

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Leave::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
+config.action_mailer.default_url_options = {:host => "localhost:3000"}
 config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
