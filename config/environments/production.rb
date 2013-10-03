286ebdf12bc651f5b996db6e60e727417566d17d
naresh1984 <naresh.nookal@gmail.com>
2013-10-03 17:31:34 +0530

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Leave::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-config.action_mailer.default_url_options = {:host => "http://pure-citadel-9384.herokuapp.com/"}
+config.action_mailer.default_url_options = {:host => "pure-citadel-9384.herokuapp.com"}
 config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
