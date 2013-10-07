71d73be649e569e6453b4ef72d3e7b263b69fbe2
naresh1984 <naresh.nookal@gmail.com>
2013-10-07 18:13:46 +0530

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ def employee_module (id)
 @search1=" AND  leave_requests.fromdate >= '#{d.at_beginning_of_month.to_formatted_s(:db)}'"
 #@employee_module=Leafe.select('leaves.*,(select count(*) From leave_requests Where  leave_requests.employee_id=leaves.employee_id  AND  leave_requests.fromdate >= "'+d.at_beginning_of_month.to_formatted_s(:db)+'" AND  leave_requests.status!="Cancelled" AND  leave_requests.status!="Commited"  ) as count').find_by_employee_id(id);
  @employee_ms=Leafe.find_by_employee_id(id);
- @employee_ms.lops=LeaveRequest.joins(:employee).where("leave_requests.fromdate >='#{d.at_beginning_of_month.to_formatted_s(:db)}' AND  leave_requests.status!='Cancelled' AND  leave_requests.status!='Commited' AND  employee_id= #{id}").count
+ @employee_ms.lops=LeaveRequest.joins(:employee).where("leave_requests.fromdate >='#{d.at_beginning_of_month.to_formatted_s(:db)}' AND  leave_requests.status!='Cancelled' AND  leave_requests.status!='Commited'  AND  leave_requests.status!='Rejected' AND  employee_id= #{id}").count
 
 return @employee_ms
 end
